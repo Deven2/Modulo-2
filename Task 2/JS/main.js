9951a4f03732bfcb3e335131bf67bfbf801ad25d
Nicolas Sauco <59623002+Deven2@users.noreply.github.com>
2020-01-28 17:48:54 -0300

    Tabla terminada, boostrap encaminado (dropdown)

--- Task 2/JS/main.js
+++ Task 2/JS/main.js
@@ -1,3 +1,4 @@
-document.getElementById("senate-data").innerHTML = JSON.stringify(data,null,2);
-
-console.log(data);
+data.results[0].members.forEach(members =>{
+	let full_name = members.last_name + ' ' + members.first_name + ' ' + (members.middle_name ? members.middle_name : '');
+	document.querySelector("tbody").innerHTML += '<tr><td>' + (members.url != '' ? '<a href= "' + members.url + '" >' + full_name + '</a>'  : full_name) + '</td><td>' + members.party + '</td><td>' + members.state + '</td><td>' + members.seniority + '</td><td>' + members.votes_with_party_pct + '%</td></tr>';
+});
