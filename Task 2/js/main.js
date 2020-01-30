14f12ba87d2a80f955037410ac81bd9f98c51f80
Nicolas Sauco <59623002+Deven2@users.noreply.github.com>
2020-01-30 16:22:20 -0300

    Hacer checkbox

--- Task 2/js/main.js
+++ Task 2/js/main.js
@@ -1,4 +1,6 @@
 data.results[0].members.forEach(members =>{
 	let full_name = members.last_name + ' ' + members.first_name + ' ' + (members.middle_name ? members.middle_name : '');
-	document.querySelector("tbody").innerHTML += '<tr><td>' + (members.url != '' ? '<a href= "' + members.url + '" >' + full_name + '</a>'  : full_name) + '</td><td>' + members.party + '</td><td>' + members.state + '</td><td>' + members.seniority + '</td><td>' + members.votes_with_party_pct + '%</td></tr>';
+
+	document.querySelector("tbody").innerHTML += '<tr><td>' + (members.url != '' ? '<a href= "' + members.url + '" >' + full_name + '</a>'  : full_name) + '</td><td class="party">' + members.party + '</td><td>' + members.state + '</td><td>' + members.seniority + '</td><td>' + members.votes_with_party_pct + '%</td></tr>';
 });
+
